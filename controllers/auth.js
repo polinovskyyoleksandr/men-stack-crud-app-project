a4d18137aca682eb19b816f6301ef99801db2d53
polino <polinovskyoleksandr1303@gmail.com>
2026-01-20 20:18:05 +0100

    trying to figure out why he's taking me always on the sign-in page

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,12 +4,12 @@ const bcrypt = require('bcrypt')
 
 const User = require('../models/user.js')
 
-router.get('/sign-in', (req, res) => {
-    res.render('auth/sign-in.ejs');
-})
-
 router.get('/sign-up', (req, res) => {
     res.render('auth/sign-up.ejs');
+})
+
+router.get('/sign-in', (req, res) => {
+    res.render('auth/sign-in.ejs');
 })
 
 router.get('/sign-out', (req, res) => {
